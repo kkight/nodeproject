5dbfd73f835e2df955ef811b5c105bb85b360b4a
Ang Li <leo.angli95@gmail.com>
2017-01-20 02:09:31 +0000

    3rd commit

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -4,16 +4,20 @@ var app=express();
 var port=process.env.PORT;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+
 app.use(express.static('bower_components'));
 
+app.set('views','./src/views');
+app.set('view engine','ejs');
+
 app.get('/',function(req,res){
-    res.send('Hello World!');
-    
+    //res.send('Hello World!');
+    res.render('index',{list:['first value','2nd value','third value']});
 });
 
 app.get('/routing',function(req,res){
     res.send('Hello Routing!');
+
     
 });
 app.listen(port,function(err){
